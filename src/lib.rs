c2a8f376f37290a866effd34b2a742093dfd4fe9
Elias <the@communist.dev>
2020-08-18 09:49:54 +0100

    https://os.phil-opp.com/paging-implementation

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,14 @@ extern crate rlibc;
 
 use core::panic::PanicInfo;
 
+#[cfg(test)]
+use bootloader::{entry_point, BootInfo};
+
 pub mod serial;
 pub mod vga_buffer;
 pub mod interrupts;
 pub mod gdt;
+pub mod memory;
 
 pub fn init() {
     gdt::init();
@@ -74,10 +78,13 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     hlt_loop();
 }
 
+
+#[cfg(test)]
+entry_point!(test_kernel_main);
+
 /// Entry point for `cargo test`
 #[cfg(test)]
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
     init();
     test_main();
     hlt_loop();
--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,33 @@
 
 extern crate rlibc;
 
+use x86_64::{structures::paging::{Page, MapperAllSizes}, VirtAddr};
 use core::panic::PanicInfo;
-use blog_os::{println, serial_println, serial_print, QemuExitCode, exit_qemu};
+use blog_os::{println, serial_println, serial_print, QemuExitCode, exit_qemu, memory};
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+use bootloader::{BootInfo, entry_point};
+
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    
+
     println!("Hello World{}", "!");
     blog_os::init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+
+    let mut frame_allocator = unsafe {
+        memory::BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
+
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    // write the string `New!` to the screen through the new mapping
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
 
     #[cfg(test)]
     test_main();
